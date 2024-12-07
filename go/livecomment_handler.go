ccd44bced7a955fb5b233b272cafdc958f27ac50
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-12-07 16:14:54 +0900

    perf: moderateHandlerのN+1解消

--- go/livecomment_handler.go
+++ go/livecomment_handler.go
@@ -370,29 +370,27 @@ func moderateHandler(c echo.Context) error {
 	}
 
 	// NGワードにヒットする過去の投稿も全削除する
-	for _, ngword := range ngwords {
-		// ライブコメント一覧取得
-		var livecomments []*LivecommentModel
-		if err := tx.SelectContext(ctx, &livecomments, "SELECT * FROM livecomments"); err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, "failed to get livecomments: "+err.Error())
-		}
+	var livecomments []*LivecommentModel
+	if err := tx.SelectContext(ctx, &livecomments, "SELECT * FROM livecomments WHERE livestream_id = ?", livestreamID); err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get livecomments: "+err.Error())
+	}
 
-		for _, livecomment := range livecomments {
-			query := `
-			DELETE FROM livecomments
-			WHERE
-			id = ? AND
-			livestream_id = ? AND
-			(SELECT COUNT(*)
-			FROM
-			(SELECT ? AS text) AS texts
-			INNER JOIN
-			(SELECT CONCAT('%', ?, '%')	AS pattern) AS patterns
-			ON texts.text LIKE patterns.pattern) >= 1;
-			`
-			if _, err := tx.ExecContext(ctx, query, livecomment.ID, livestreamID, livecomment.Comment, ngword.Word); err != nil {
-				return echo.NewHTTPError(http.StatusInternalServerError, "failed to delete old livecomments that hit spams: "+err.Error())
-			}
+	// アプリ側でNGワードにヒットするコメントを削除する
+	var deletedLivecommentsIDs []int64
+	for _, livecomment := range livecomments {
+		if strings.Contains(livecomment.Comment, req.NGWord) {
+			deletedLivecommentsIDs = append(deletedLivecommentsIDs, livecomment.ID)
+		}
+	}
+
+	if len(deletedLivecommentsIDs) > 0 {
+		query, args, err := sqlx.In("DELETE FROM livecomments WHERE id IN (?)", deletedLivecommentsIDs)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, "failed to delete old livecomments that hit spams: "+err.Error())
+		}
+		query = tx.Rebind(query)
+		if _, err := tx.ExecContext(ctx, query, args...); err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, "failed to delete old livecomments that hit spams: "+err.Error())
 		}
 	}
 
